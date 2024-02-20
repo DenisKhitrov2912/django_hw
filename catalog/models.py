09aabf5bb5d1b57a9e06de50065101d6a3c3570e
Khitrov <Hitrov.95@yandex.ru>
2024-02-20 17:52:38 +0300

    appended "manufactured_at"

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, verbose_name='имя')
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        print(f"{self.name}")
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name='имя')
+    description = models.TextField(verbose_name='описание')
+    image = models.ImageField(upload_to='products/', verbose_name='превью', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    cost = models.IntegerField(verbose_name='цена')
+    created_at = models.DateField(verbose_name='дата создания')
+    updated_at = models.DateField(verbose_name='дата изменения')
+    manufactured_at = models.DateField(verbose_name='дата производства', **NULLABLE)
+
+    def __str__(self):
+        print(f"{self.name}, {self.cost}")
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'my_db_django',
+        'USER': 'postgres',
+        'PASSWORD': '1234567'
     }
 }
 
@@ -118,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
