88f5f61ff2e9675f949f93aa4287082773d79734
Khitrov <Hitrov.95@yandex.ru>
2024-02-22 14:47:03 +0300

    end of work (finally)

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,7 +5,7 @@ from catalog.views import contacts, home, products, add_product, product_list
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('products/<pk>/', products, name='products'),
+    path('products/<int:pk>/', products, name='products'),
     path('add_product/', add_product, name='add_product'),
     path('product_list/', product_list, name='product_list')
 ]
